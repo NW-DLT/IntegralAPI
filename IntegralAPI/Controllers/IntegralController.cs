35bf28d155cb2d60be7f6ad28fc7f96d6697ec2c
NW_DLT <alexputil228@mail.ru>
2023-11-19 23:06:40 +0300

    Добавьте файлы проекта.

---
+++ IntegralAPI/Controllers/IntegralController.cs
@@ -0,0 +1,26 @@
+﻿using IntegralAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegralAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IntegralController : Controller
+    {
+        [HttpGet("calculateIntegral")]
+        public async Task<ActionResult<double>> CalculateIntegral(int SplitNumbers, double UpLim, double LowLim)
+        {
+            try
+            {
+                SimsonMethod simsonMethod = new SimsonMethod(SplitNumbers, UpLim, LowLim);
+                simsonMethod.Calculate();
+                return simsonMethod.res;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
